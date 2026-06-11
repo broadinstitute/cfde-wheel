d391a8309291551db16418d7a594526f7fb40e1c
Alex Shilin <shilin@broadinstitute.org>
2026-06-11 11:36:24 -0400

    DCC links

--- src/data.js
+++ src/data.js
@@ -2,91 +2,91 @@ export const DEFAULT_DCCS = [
   {
     id: "65af85ae-82d5-5b81-bc66-6bddaa6420ce",
     short_label: "Kids First",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=Kids%20First",
+    homepage: "https://portal.kidsfirstdrc.org/",
     icon: "assets/kids-first.png",
     description: "Data, tools, and resources empowering pediatric research"
   },
   {
     id: "e332dadd-8084-5fbc-be41-29d75775aab3",
     short_label: "A2CPS",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=A2CPS",
+    homepage: "https://a2cps.org/",
     icon: "assets/a2cps.png",
     description: "Understanding the complex biological processes underlying chronic pain"
   },
   {
     id: "803ad44d-e7a2-550a-95c6-57855bf06be8",
     short_label: "HuBMAP",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=HuBMAP",
+    homepage: "https://hubmapconsortium.org/",
     icon: "assets/hubmap.png",
     description: "Cellular spatial atlas of the human body"
   },
   {
     id: "d6bb00c3-7224-5001-b9c5-9838622fba40",
     short_label: "4DN",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=4DN",
+    homepage: "https://www.4dnucleome.org/",
     icon: "assets/4dn.png",
     description: "Nuclear organization in space and time"
   },
   {
     id: "f3f490cf-fd69-579c-8ea3-472c7cf3fb59",
     short_label: "LINCS",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=LINCS",
+    homepage: "https://lincsproject.org/LINCS/",
     icon: "assets/lincs-logo.png",
     description: "Omics signatures for drug & target discovery"
   },
   {
     id: "a1289ebb-0306-59a1-b0fc-e4d03a4790d7",
     short_label: "IDG",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=IDG",
+    homepage: "https://druggablegenome.net/",
     icon: "assets/idg.png",
     description: "Illuminating GPCRs, kinases, ion channels, & other drug targets"
   },
   {
     id: undefined,
     short_label: "NPH",
-    homepage: "https://commonfund.nih.gov/nutritionforprecisionhealth",
+    homepage: "https://nutritionforprecisionhealth.org/",
     icon: "assets/nph.png",
     description: "Predictive algorithms to advance nutrition research"
   },
   {
     id: "e31052b0-ac50-5ede-9828-698ff3610427",
     short_label: "GlyGen",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=GlyGen",
+    homepage: "https://www.glygen.org/",
     icon: "assets/glygen.png",
     description: "Computational and informatics resources for glycoscience"
   },
   {
     id: "75b3be39-a021-5d80-b7e2-2a7938a1e11a",
     short_label: "Bridge2AI",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=Bridge2AI",
+    homepage: "https://bridge2ai.org/",
     icon: "assets/bridge2ai.png",
     description: "Biomedical AI ↔ people, data & ethics"
   },
   {
     id: "a9aeab22-4fbc-5329-aef6-21110f463c23",
     short_label: "MoTrPAC",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=MoTrPAC",
+    homepage: "https://motrpac-data.org/",
     icon: "assets/motrpac.png",
     description: "The molecular map of exercise"
   },
   {
     id: "089d8d63-3364-526f-9706-80d62d0ec88c",
     short_label: "Metabolomics Workbench",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=Metabolomics",
+    homepage: "https://www.metabolomicsworkbench.org/",
     icon: "assets/metabolomics.png",
     description: "Metabolomics"
   },
   {
     id: undefined,
     short_label: "SCGE",
-    homepage: "https://commonfund.nih.gov/editing",
+    homepage: "https://scge.mcw.edu/",
     icon: "assets/scge.png",
     description: "Reducing the burden of diseases caused by genetic changes"
   },
   {
     id: "2399794e-74c6-5735-a039-0782cdeeb1e2",
     short_label: "SPARC",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=SPARC",
+    homepage: "https://sparc.science/",
     icon: "assets/sparc.svg",
     description: "Bridging the body and brain"
   },
@@ -100,21 +100,21 @@ export const DEFAULT_DCCS = [
   {
     id: "b3028db2-209c-5862-8f4d-33c5b312332e",
     short_label: "GTEx",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=GTEx",
+    homepage: "https://www.gtexportal.org/home/",
     icon: "assets/gtex.png",
     description: "Gene expression and regulation across human tissues"
   },
   {
     id: "dd66e8a5-0e05-5a43-a0ca-18cc3698bb36",
     short_label: "SenNet",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=SenNet",
+    homepage: "https://sennetconsortium.org/",
     icon: "assets/sennet.png",
     description: "Mapping senescent cells"
   },
   {
     id: "f65babf7-2875-5725-9635-210d654533f1",
     short_label: "ExRNA",
-    homepage: "https://cfdeknowledge.org/r/kc_programs?DCC=exRNA",
+    homepage: "https://exrna.org/",
     icon: "assets/exrna.png",
     description: "Extracellular RNA communication"
   }
